9e63a288b48041be21838948a2ee25433a68a19b
PD <dave.sfsu@gmail.com>
2017-02-06 01:03:29 -0800

    Added a way to store the current user

--- TwitterClone/User.swift
+++ TwitterClone/User.swift
@@ -14,9 +14,13 @@ class User: NSObject {
     var screenname: NSString?
     var profileUrl: NSURL?
     var tagline: NSString?
-
+    
+    var dictionary: NSDictionary?
+    
     
     init(dictionary: NSDictionary){
+        
+        self.dictionary = dictionary
         
         name = dictionary["name"] as! NSString?
         screenname = dictionary["screen_name"] as! NSString?
@@ -30,4 +34,41 @@ class User: NSObject {
         tagline = dictionary["description"] as! NSString?
         
     }
+    
+    static var _currentUser: User?
+    
+    class var currentUser: User? {
+        get {
+            if _currentUser == nil {
+                let userDefault = UserDefaults.standard
+                let userData = userDefault.object(forKey: "currentUserData") as? Data
+                
+                if let userData = userData {
+                    let dictionary = try!
+                        JSONSerialization.jsonObject(with: userData, options: []) as! NSDictionary
+                    
+                    _currentUser = User(dictionary: dictionary)
+                }
+            }
+            return _currentUser
+        }
+        
+        set(user) {
+            _currentUser = user
+            
+            let userDefault = UserDefaults.standard
+            
+            if let user = user {
+                let data = try! JSONSerialization.data(withJSONObject: user.dictionary!, options: [])
+                userDefault.set(data, forKey: "currentUserData")
+            } else {
+                userDefault.set(nil, forKey: "currentUserData")
+            }
+            
+            userDefault.set(user, forKey: "currentUser")
+            
+            userDefault.synchronize()
+        }
+    }
+    
 }
