faa883dff6127d51171f94ae54ee1a9768a60339
PD <dave.sfsu@gmail.com>
2017-02-08 00:40:14 -0800

    Created more comments and cleaned up the code further

--- TwitterClone/TweetCell.swift
+++ TwitterClone/TweetCell.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class TweetCell: UITableViewCell {
     
+    //Outlet for the view
     @IBOutlet weak var tweetCell: UIView!
     
+    //Outlets
     @IBOutlet weak var pictureImageView: UIImageView!
     @IBOutlet weak var nameButton: UIButton!
     @IBOutlet weak var screennameButton: UIButton!
@@ -23,27 +25,34 @@ class TweetCell: UITableViewCell {
     
     let dateformatter = DateFormatter()
 
+    //individual tweet cell
     var tweet: Tweet! {
         
         didSet {
             
+            //Changed the name
             nameButton.setTitle(tweet.user?.name as String?, for: .normal)
             
+            //Changd the screenname
             screennameButton.setTitle("@" + (tweet.user!.screenname as String!)!, for: .normal)
             
+            //Changed the tweet text
             tweetLabel.text = tweet.text
             
+            //Safely unwrap the profileURL and then set the image
             if let profileURL = tweet.user?.profileUrl{
                 pictureImageView.setImageWith(profileURL as URL)
             }
             
+            //Set the dateformat
             dateformatter.dateFormat = "MMM d, h:mm a"
             
+            //Set the date
             timestampButton.setTitle(dateformatter.string(from: tweet.timestamp!), for: .normal)
 
+            //Set the favorite count
             likeButton.setTitle("\(tweet.favoritesCount)", for: .normal)
 
-            
         }
     }
 
