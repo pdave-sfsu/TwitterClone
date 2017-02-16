a7f41f564d1afb98b0aeb6a3591f64ab0e567ce0
PD <dave.sfsu@gmail.com>
2017-02-16 09:41:35 -0800

    Added TweetDetailViewController

--- TwitterClone/TweetsViewController.swift
+++ TwitterClone/TweetsViewController.swift
@@ -75,15 +75,19 @@ class TweetsViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return cell
     }
     
-    /*
+    
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destinationViewController.
      // Pass the selected object to the new view controller.
+        
+        print("Holy shit this worked")
+        
+        
      }
-     */
+ 
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
